3522409f7da73376d77c2761b7be6ed706eee1de
Malar <malarvizhiravichandran8@gmail.com>
2025-04-08 21:59:50 +0530

    Name change

--- Movie Hunter/src/components/Header.jsx
+++ Movie Hunter/src/components/Header.jsx
@@ -13,7 +13,7 @@ export const Header = () => {
     <>
     <nav className="navbar navbar-expand-md fixed-top bg-primary navbar-dark">
       <div className="container-fluid">
-        <NavLink to="/" className="navbar-brand"><i className="bi bi-film"></i> Movie Hunter</NavLink>
+        <NavLink to="/" className="navbar-brand"><i className="bi bi-film"></i> Naveen's Movie Hunter</NavLink>
         
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#menu">
           <span className="navbar-toggler-icon"></span>
